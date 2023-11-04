55eeaf1db70002f163f28e206247a702d77fa359
cyber-shah <pranchx@gmail.com>
2023-11-04 09:45:00 -0400

    before swithcing theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,6 @@ export default function Navbar(props) {
                 <Tab label="About" />
                 <Tab label="Works" />
                 <Tab label="Contact" />
-                <Tab label="Links" />
             </Tabs>
         </Box>
     </>
