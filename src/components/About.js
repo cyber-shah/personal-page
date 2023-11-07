d4df292b407797bea42677e5e840d08bf0ecb936
cyber-shah <pranchx@gmail.com>
2023-11-07 00:00:37 -0500

    more procedures

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     const styles = {
       main: {
         height : '100vh',
-        width : '80vw',
+        'max-width' : '1200px',
         padding: '20px',
         margin: '0 auto',
         display: 'flex',
@@ -18,10 +18,6 @@ export default function About() {
         'text-align': 'left',
         flex: 3,
       },
-      thirds: {
-        'text-align': 'left',
-        flex: 1,
-      }
     }
   return (
     <div className='main' style={styles.main} >
@@ -42,11 +38,26 @@ export default function About() {
               <br/> Concentration: Systems and Software	
               <br/> Related Courses: Database Management Systems, Algorithms, Scalable Distributed Systems.
             </Typography>
+
+
+
+            <br/><br/>
+            <Typography variant='h6'>
+              Harvard University, Cambridge, MA	
+              <br/> MS in Computer Science - Align 
+            </Typography>
         </div>
 
         <div style={styles.thirds}> 
           <Typography variant='body1' sx={{ textAlign: 'right' }}>
-            May 2025 (Expected) <br/> GPA: 4.00
+            May 2025 <br/> 
+            (Expected) <br/> 
+            GPA: 4.00 <br/>
+            
+            <br/><br/><br/><br/><br/><br/>
+            
+            December 2022<br/> 
+            (Incomplete)
           </Typography>    
         </div>
 
